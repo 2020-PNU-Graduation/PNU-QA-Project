82228df1048c63cf2dde7d079dbb37e1a5067eba
TaeHyeong Kim <uuzaza@naver.com>
2020-07-17 02:52:53 +0900

    python project init, import modules

---
+++ venv/bin/app/sourceCode/Table_Holder.py
@@ -0,0 +1,134 @@
+import Cuncker
+
+import numpy as np
+
+from itertools import product
+from bs4 import BeautifulSoup
+
+
+def table_to_2d(table_tag):
+    rowspans = []  # track pending rowspans
+    rows = table_tag.find_all('tr')
+
+    # first scan, see how many columns we need
+    colcount = 0
+    for r, row in enumerate(rows):
+        cells = row.find_all(['td', 'th'], recursive=False)
+        # count columns (including spanned).
+        # add active rowspans from preceding rows
+        # we *ignore* the colspan value on the last cell, to prevent
+        # creating 'phantom' columns with no actual cells, only extended
+        # colspans. This is achieved by hardcoding the last cell width as 1.
+        # a colspan of 0 means “fill until the end” but can really only apply
+        # to the last cell; ignore it elsewhere.
+        colcount = max(
+            colcount,
+            sum(int(c.get('colspan', 1)) or 1 for c in cells[:-1]) + len(cells[-1:]) + len(rowspans))
+        # update rowspan bookkeeping; 0 is a span to the bottom.
+        rowspans += [int(c.get('rowspan', 1)) or len(rows) - r for c in cells]
+        rowspans = [s - 1 for s in rowspans if s > 1]
+
+    # it doesn't matter if there are still rowspan numbers 'active'; no extra
+    # rows to show in the table means the larger than 1 rowspan numbers in the
+    # last table row are ignored.
+
+    # build an empty matrix for all possible cells
+    table = [[None] * colcount for row in rows]
+
+    # fill matrix from row data
+    rowspans = {}  # track pending rowspans, column number mapping to count
+    for row, row_elem in enumerate(rows):
+        span_offset = 0  # how many columns are skipped due to row and colspans
+        for col, cell in enumerate(row_elem.find_all(['td', 'th'], recursive=False)):
+            # adjust for preceding row and colspans
+            col += span_offset
+            while rowspans.get(col, 0):
+                span_offset += 1
+                col += 1
+
+            # fill table data
+            rowspan = rowspans[col] = int(cell.get('rowspan', 1)) or len(rows) - row
+            colspan = int(cell.get('colspan', 1)) or colcount - col
+            # next column is offset by the colspan
+            span_offset += colspan - 1
+            value = cell.get_text()
+            for drow, dcol in product(range(rowspan), range(colspan)):
+                try:
+                    table[row + drow][col + dcol] = value
+                    rowspans[col + dcol] = rowspan
+                except IndexError:
+                    # rowspan or colspan outside the confines of the table
+                    pass
+
+        # update rowspan bookkeeping
+        rowspans = {c: s - 1 for c, s in rowspans.items() if s > 1}
+
+    return table
+
+
+class Holder:
+    def __init__(self):
+        None
+
+    def get_table_text(self, table_text):
+        table2 = BeautifulSoup(table_text, 'html.parser')
+
+        try:
+            table_data_list = table_to_2d(table_tag=table2)
+
+            tr_lines = table_text.split('<tr>')
+            tr_lines.pop(0)
+
+            table_heads = []
+            table_data = []
+
+            if len(tr_lines) == len(table_data_list):
+                for i in range(len(tr_lines)):
+                    if tr_lines[i].find('<th') != -1:
+                        table_heads.append(table_data_list[i])
+                    else:
+                        table_data.append(table_data_list[i])
+            # print(len(table_heads), len(table_data))
+            if len(table_heads) > 0:
+                self.table_head = table_heads[-1]
+            else:
+                self.table_head = table_data[0]
+            self.table_data = table_data
+
+            transposed_table = True
+            for line in tr_lines:
+                if line.find('<th') == -1:
+                    transposed_table = False
+
+            if transposed_table is True:
+                self.table_head = []
+                self.table_data = []
+                for table_data in table_data_list:
+                    head_word = table_data.pop(0)
+                    self.table_head.append(head_word)
+                    self.table_data.append(table_data)
+
+        except:
+            self.table_head = []
+            self.table_data = []
+            return
+
+    def get_data_line(self, question):
+        chuncker = Chuncker.Chuncker()
+        chuncker.get_feautre(question)
+
+        scores = []
+
+        for data in self.table_data:
+            string = ''
+            for word in data:
+                if word is not None:
+                    string += word + ' '
+
+            scores.append(chuncker.get_chunk_score(string))
+
+        if len(scores) == 0:
+            return 0
+
+        return np.array(scores, dtype=np.float32).argmax()
+
---
+++ venv/bin/app/sourceCode/get_table_data.py
@@ -0,0 +1,299 @@
+from HTML_Utils import *
+import os
+import json
+import os
+
+import tokenization
+import numpy as np
+
+import Chuncker
+
+
+chuncker = Chuncker.Chuncker()
+
+max_length = 450
+
+path_dir = 'F:\\korquad2_data'
+
+sequence_has_ans = np.zeros(shape=[80000 * 2, max_length], dtype=np.int32)
+segments_has_ans = np.zeros(shape=[80000 * 2, max_length], dtype=np.int32)
+mask_has_ans = np.zeros(shape=[80000 * 2, max_length], dtype=np.int32)
+answer_span = np.zeros(shape=[80000 * 2, 2], dtype=np.int32)
+paragraph_type = np.zeros(shape=[80000 * 2], dtype=np.int32)
+
+sequence_has_ans_z = np.zeros(shape=[80000 * 2, 3, max_length], dtype=np.int32)
+segments_has_ans_z = np.zeros(shape=[80000 * 2, 3, max_length], dtype=np.int32)
+mask_has_ans_z = np.zeros(shape=[80000 * 2, 3, max_length], dtype=np.int32)
+
+file_list = os.listdir(path_dir)
+file_list.sort()
+
+file_list.pop(len(file_list) - 1)
+file_list.pop(len(file_list) - 1)
+
+print(file_list)
+data_num = 0
+
+vocab = tokenization.load_vocab(vocab_file='html_mecab_vocab_128000.txt')
+tokenizer = tokenization.WordpieceTokenizer(vocab=vocab)
+
+count = 0
+false_count = 0
+false_count2 = 0
+
+cor = 0
+wrong_case = 0
+
+for file_name in file_list:
+
+    print(file_name, 'processing....', data_num)
+
+    in_path = path_dir + '\\' + file_name
+    data = json.load(open(in_path, 'r', encoding='utf-8'))
+
+    for article in data['data']:
+        doc = str(article['context'])
+
+        print(count, false_count, false_count2, file_name)
+
+        for qas in article['qas']:
+            error_code = -1
+
+            answer = qas['answer']
+            answer_start = answer['answer_start']
+            answer_text = answer['text']
+            question = qas['question']
+
+            chuncker.get_feautre(query=question)
+
+            query_tokens = []
+            query_tokens.append('[CLS]')
+            q_tokens = tokenizer.tokenize(question.lower())
+            for tk in q_tokens:
+                query_tokens.append(tk)
+            query_tokens.append('[SEP]')
+
+            doc_ = list(doc)
+
+            ans1 = ''
+            ans2 = ''
+
+            ###부정확한 케이스는 제거
+            right_case = True
+
+            if len(doc) < answer_start + len(answer_text):
+                right_case = False
+                error_code = 1
+
+            if len(doc) < answer_start:
+                right_case = False
+                error_code = 1
+            ###
+
+            ######
+            # 정답에 ans 토큰을 임베딩하기 위한 코드
+            ######
+            if len(answer_text) > 1 and right_case is True:
+
+                if doc[answer_start - 1] == ' ':
+                    doc_ = doc[0: answer_start] + ' [answer] ' + answer_text + doc[answer_start + len(answer_text): -1]
+                else:
+                    doc_ = doc[0: answer_start] + ' [answer]' + answer_text + doc[answer_start + len(answer_text): -1]
+
+            else:
+                right_case = False
+                error_code = 3
+            ########
+
+            ########
+            #html 태그를 전처리 하기 위한 코드
+            ########
+            if right_case is False:
+                print('wrong case!!', error_code)
+                wrong_case += 1
+
+            if right_case is True:
+                doc_, table_list = pre_process_document(doc_, answer_setting=False, a_token1=ans1, a_token2=ans2,
+                                                        td_setting=True)
+
+                doc_ = doc_.replace('[a]', '')
+                doc_ = doc_.replace('[/a]', '')
+                doc_ = doc_.replace('[/li]', '')
+                doc_ = doc_.replace('[/td]', '')
+                doc_ = doc_.replace('[/th]', '')
+
+                #######
+                # 너무 긴 Token을 가지는 입력(512>)에 대해서 여러개의 입력으로 split 하는 코드입니다
+                #######
+                tokens = tokenizer.tokenize(doc_.lower())
+
+                #get text seperated with tags
+                ###
+                #
+                tagged_texts = []
+                tabluar_texts = []
+
+                seq = ''
+                selected = -1
+
+                for i in range(len(tokens)):
+                    seq += tokens[i] + ' '
+
+                    if (tokens[i] == '[/table]' or tokens[i] == '[/p]' or
+                        tokens[i] == '[/list]' or tokens[i] == '##[/p]' or
+                        tokens[i] == '##[/table]' or tokens[i] == '##[/list]') is True:
+
+                        if seq.find('[answer]') != -1:
+                            selected = len(tagged_texts)
+                            answer_seq = seq
+
+                        tagged_texts.append(seq.strip())
+                        seq = ''
+                ########
+
+                i = 0
+                while True:
+                    if i >= len(tagged_texts):
+                        break
+
+                    if tagged_texts[i].find('table') == -1:
+                        table_text = tagged_texts.pop(i)
+
+                        # sequences.append(table_text)
+                    else:
+                        i += 1
+                #
+                ###
+                selected = -1
+                for i in range(len(tagged_texts)):
+                    if tagged_texts[i].find('[answer]') != -1:
+                        selected = i
+
+                if selected == -1:
+                    continue
+
+                if tagged_texts[selected].find('[table]') == -1:
+                    continue
+
+                sequences = tagged_texts
+
+                ch_scores = []
+
+                for sequence in sequences:
+                    if sequence.find('[answer]') == -1:
+                        if sequence.find('[table]') != -1:
+                            ch_scores.append(chuncker.get_chunk_score(paragraph=sequence) + 100)
+                        else:
+                            ch_scores.append(chuncker.get_chunk_score(paragraph=sequence))
+                        #continue
+                    else:
+                        ch_scores.append(-9999)
+
+                ch_scores = np.array(ch_scores, dtype=np.float32)
+                #####
+
+                for sequence in sequences:
+                    if sequence.find('[answer]') == -1:
+                        continue
+
+                    for k in range(3):
+                        zero_sequence = sequences[ch_scores.argmax()]
+                        zero_sequence = zero_sequence.split('검색 ##하 ##러 가 ##기')[-1]
+                        ch_scores[ch_scores.argmax()] -= 100
+                        #print(zero_sequence)
+
+                        tokens = []
+                        segments_zero = []
+
+                        tag_text = zero_sequence
+                        tag_text = tag_text.replace('[/th]', '')
+                        tag_text = tag_text.replace('[/td]', '')
+                        tag_text = tag_text.replace('  ', ' ')
+                        tag_text = tag_text.replace('  ', ' ')
+
+                        tokens_ = zero_sequence.strip().split(' ')
+                        for tk in query_tokens:
+                            tokens.append(tk)
+                            segments_zero.append(0)
+                        for tk in tokens_:
+                            tokens.append(tk)
+                            segments_zero.append(1)
+
+                        ids_zero = tokenization.convert_tokens_to_ids(tokens=tokens, vocab=vocab)
+                        length = len(ids_zero)
+                        if length > max_length:
+                            length = max_length
+                        for j in range(length):
+                            sequence_has_ans_z[count, k, j] = ids_zero[j]
+                            segments_has_ans_z[count, k, j] = segments_zero[j]
+                            mask_has_ans_z[count, k, j] = 1
+
+                    tag_text = sequence.split('검색 ##하 ##러 가 ##기')[-1]
+
+                    tag_text = tag_text.replace('[/th]', '')
+                    tag_text = tag_text.replace('[/td]', '')
+                    tag_text = tag_text.replace('[answer]', '')
+                    tag_text = tag_text.replace('[/answer]', '')
+
+                    tag_text = tag_text.replace('  ', ' ')
+                    tag_text = tag_text.replace('  ', ' ')
+                    #print(tag_text)
+                    #input()
+                    tokens = []
+                    segments = []
+
+                    tokens_ = tag_text.strip().split(' ')
+                    for tk in query_tokens:
+                        tokens.append(tk)
+                        segments.append(0)
+                    for tk in tokens_:
+                        if tk == '[answer]':
+                            start_idx = len(tokens)
+                        elif tk == '[/answer]':
+                            stop_idx = len(tokens) - 1
+                        else:
+                            tokens.append(tk)
+                            segments.append(1)
+
+                    ids = tokenization.convert_tokens_to_ids(tokens=tokens, vocab=vocab)
+                    length = len(ids)
+                    if length > max_length:
+                        length = max_length
+                    for j in range(length):
+                        sequence_has_ans[count, j] = ids[j]
+                        segments_has_ans[count, j] = segments[j]
+                        mask_has_ans[count, j] = 1
+                    count += 1
+                    #####################
+
+sequence_has_ans_ = np.zeros(shape=[count, max_length], dtype=np.int32)
+segments_has_ans_ = np.zeros(shape=[count, max_length], dtype=np.int32)
+mask_has_ans_ = np.zeros(shape=[count, max_length], dtype=np.int32)
+answer_span_ = np.zeros(shape=[count, 2], dtype=np.int32)
+paragraph_type_ = np.zeros(shape=[count], dtype=np.int32)
+
+sequence_has_ans_z_ = np.zeros(shape=[count, 3, max_length], dtype=np.int32)
+segments_has_ans_z_ = np.zeros(shape=[count, 3, max_length], dtype=np.int32)
+mask_has_ans_z_ = np.zeros(shape=[count, 3, max_length], dtype=np.int32)
+
+for i in range(count):
+    sequence_has_ans_[i] = sequence_has_ans[i]
+    segments_has_ans_[i] = segments_has_ans[i]
+    mask_has_ans_[i] = mask_has_ans[i]
+    answer_span_[i] = answer_span[i]
+    paragraph_type_[i] = paragraph_type[i]
+
+    sequence_has_ans_z_[i] = sequence_has_ans_z[i]
+    segments_has_ans_z_[i] = segments_has_ans_z[i]
+    mask_has_ans_z_[i] = mask_has_ans_z[i]
+
+np.save('sequence_table_z', sequence_has_ans_z_)
+np.save('segments_table_z', segments_has_ans_z_)
+np.save('mask_table_z', mask_has_ans_z_)
+
+np.save('sequence_table', sequence_has_ans_)
+np.save('segments_table', segments_has_ans_)
+np.save('mask_table', mask_has_ans_)
+np.save('answer_span_table', answer_span_)
+np.save('paragraph_type_table', paragraph_type_)
+
