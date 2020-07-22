145598f8d2ed2c9034df368d0a0d6bc04bb02ab5
Kim_TaeHyeong <37135317+KimTaeHyeong17@users.noreply.github.com>
2020-07-23 00:49:12 +0900

    프로젝트 다시만듬

---
+++ PNU_QA_Project/MecabTest.py
@@ -0,0 +1,13 @@
+from eunjeon import Mecab
+
+tagger = Mecab()
+
+sentence = '아무문장이다.'
+print(tagger.morphs(sentence))
+
+#아나콘다 환경
+#python 3.6
+#대소 비교가 가능한 숫자(뭐 ~km) 같은거 2차언 배열로 순위 매기기
+#대소 비교 불가능한 건 0 으로 처리하고 1순위부터 시작해서 2차원 배열 생성
+
+# '/usr/local/lib/mecab/dic/mecab-ko-dic'
---
+++ PNU_QA_Project/example.py
@@ -0,0 +1,20 @@
+from HTML_Utils import get_table_head
+import re
+import Table_Holder
+from HTML_Utils import get_table_head
+from HTML_Utils import overlap_table_process
+
+import numpy as np
+
+table_holder = Table_Holder.Holder()
+
+file = open('test.html', 'r', encoding='utf-8')
+table_text = file.read()
+
+table_text, overlap_table_texts = overlap_table_process(table_text=table_text)
+
+table_holder.get_table_text(table_text=table_text)
+table_data = table_holder.table_data
+
+for data in table_data:
+    print(data)
